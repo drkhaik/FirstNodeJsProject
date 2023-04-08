920f23f90056f19ca1e8ed37fb99fe4a4e1954b0
drkhaikDev <trinhkhaiviet@gmail.com>
2023-04-08 22:19:52 +0700

    second commit

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
 require('dotenv').config();
+import connectDB from "./config/connectDB";
+
+
 // import dotenv from "dotenv";
 
 let app = Express();
@@ -12,6 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+
+connectDB();
 
 let port = process.env.PORT || 6969;
 // port === undefined => port = 6969
